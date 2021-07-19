94cc1601b8c01a4326ac81762c9fd2086e20f7f9
Mark <mark.stevens@infinityworks.com>
2021-07-19 09:15:25 +0100

    linting
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- steps/mandatory_reconsideration.sh
+++ steps/mandatory_reconsideration.sh
@@ -5,10 +5,8 @@ set -Eeuo pipefail
     # Import the logging functions
     source /opt/emr/logging.sh
 
-    source /var/ci/resume_step.sh
-
     function log_wrapper_message() {
-        log_aws_uc_feature_message "$${1}" "mandatory_reconsideration.sh" "Running as: ,$USER"
+        log_aws_uc_feature_message "$${1}" "mandatory_reconsideration.sh" "Running as: $USER"
     }
 
     UC_FEATURE_LOCATION="${uc_feature_scripts_location}" 
@@ -27,6 +25,7 @@ set -Eeuo pipefail
     log_wrapper_message "Set the following. published_bucket: $PUBLISHED_BUCKET, target_db: $TARGET_DB, serde: $SERDE, lazy_serde: $LAZY_SERDE, sql_dir: $SQL_DIR, uc_feature_dir: $UC_FEATURE_LOCATION"
 
     log_wrapper_message "Starting mandatory_reconsideration job"
+
 
     "$UC_FEATURE_LOCATION"/mandatory_reconsideration/mandatory_reconsideration.sh "$TARGET_DB" "$S3_PREFIX" "$SERDE" "$LAZY_SERDE" "$SQL_DIR"
 
