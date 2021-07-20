b4dac9b207fd5ddd2fb6ece8629c027fa69c2bef
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-07-20 16:26:08 +0100

    bump version (#10)
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
    
    Co-authored-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- locals.tf
+++ locals.tf
@@ -74,11 +74,11 @@ locals {
   }
 
   aws_uc_feature_version = {
-    development = "0.0.6"
-    qa          = "0.0.6"
-    integration = "0.0.6"
-    preprod     = "0.0.6"
-    production  = "0.0.6"
+    development = "0.0.7"
+    qa          = "0.0.7"
+    integration = "0.0.7"
+    preprod     = "0.0.7"
+    production  = "0.0.7"
   }
 
   aws_uc_feature_alerts = {
