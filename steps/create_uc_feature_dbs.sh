862b47273c68a42e0cbe1ef7d59bb01a91219ec9
Mark <mark.stevens@infinityworks.com>
2021-08-12 10:25:07 +0100

    removed commented out code
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- steps/create_uc_feature_dbs.sh
+++ steps/create_uc_feature_dbs.sh
@@ -10,17 +10,6 @@ set -Eeuo pipefail
         log_aws_uc_feature_message "$${1}" "create-uc-features-dbs.sh" "Running as: ,$USER"
     }
 
-
-    # CORRELATION_ID="$2"
-    # S3_PREFIX="$4"
-    # SNAPSHOT_TYPE="$6"
-    # EXPORT_DATE="$8"
-
-    # echo "$CORRELATION_ID" >>     /opt/emr/correlation_id.txt
-    # echo "$S3_PREFIX" >>          /opt/emr/s3_prefix.txt
-    # echo "$SNAPSHOT_TYPE" >>      /opt/emr/snapshot_type.txt
-    # echo "$EXPORT_DATE" >>        /opt/emr/export_date.txt
-
     log_wrapper_message "Creating UC Feature Databases"
 
     hive -e "CREATE DATABASE IF NOT EXISTS ${uc_feature_db} LOCATION '${published_bucket}/${hive_metastore_location}';"
