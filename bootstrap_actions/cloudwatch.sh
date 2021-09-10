3cbcbf03e4cd31d926d2826b296496a238198a44
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-09-10 16:46:27 +0100

    Log group

--- bootstrap_actions/cloudwatch.sh
+++ bootstrap_actions/cloudwatch.sh
@@ -103,7 +103,7 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGEN
           },
           {
             "file_path": "/var/log/aws_uc_feature/e2e.log",
-            "log_group_name": "$${cwa_tests_loggrp_name}",
+            "log_group_name": "$${cwa_steps_loggrp_name}",
             "log_stream_name": "{instance_id}-e2e.log",
             "timezone": "UTC"
           }
