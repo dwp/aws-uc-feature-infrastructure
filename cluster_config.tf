a7372a2c4eff95031777f6a0c98e07e1b4f745b9
rorybryett <rory.bryett@dwp.gov.uk>
2023-04-03 10:21:24 +0100

    DTWRKS-986 propagate HCS tagging to EMR cluster

--- cluster_config.tf
+++ cluster_config.tf
@@ -13,15 +13,19 @@ resource "aws_s3_bucket_object" "cluster" {
   key    = "emr/aws_uc_feature/cluster.yaml"
   content = templatefile("${path.module}/cluster_config/cluster.yaml.tpl",
     {
-      emr_cluster_name       = local.emr_cluster_name
-      s3_log_bucket          = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
-      s3_log_prefix          = local.s3_log_prefix
-      ami_id                 = var.emr_ami_id
-      service_role           = aws_iam_role.aws_uc_feature_emr_service.arn
-      instance_profile       = aws_iam_instance_profile.aws_uc_feature.arn
-      security_configuration = aws_emr_security_configuration.ebs_emrfs_em.id
-      emr_release            = var.emr_release[local.environment]
-      environment_tag_value  = local.common_repo_tags.Environment
+      emr_cluster_name           = local.emr_cluster_name
+      s3_log_bucket              = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
+      s3_log_prefix              = local.s3_log_prefix
+      ami_id                     = var.emr_ami_id
+      service_role               = aws_iam_role.aws_uc_feature_emr_service.arn
+      instance_profile           = aws_iam_instance_profile.aws_uc_feature.arn
+      security_configuration     = aws_emr_security_configuration.ebs_emrfs_em.id
+      emr_release                = var.emr_release[local.environment]
+      dwx_environment_tag_value  = local.common_repo_tags.Environment
+      application_tag_value      = data.aws_default_tags.provider_tags.tags.Application
+      function_tag_value         = data.aws_default_tags.provider_tags.tags.Function
+      business_project_tag_value = data.aws_default_tags.provider_tags.tags.Business-Project
+      environment_tag_value      = data.aws_default_tags.provider_tags.tags.Environment
     }
   )
   tags = {
