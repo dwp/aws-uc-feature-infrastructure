c045f58a3d708917e9272df73c1e69006bc38dc9
jonnym <jon.melia@cloudadept.co.uk>
2023-08-16 11:47:19 +0100

    chore: Tanium sg and vopce config (#52)
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- security_groups.tf
+++ security_groups.tf
@@ -100,43 +100,43 @@ resource "aws_security_group_rule" "ingress_internet_proxy" {
 }
 
 resource "aws_security_group_rule" "uc_feature_host_outbound_tanium_1" {
-  description       = "UC Feature host outbound port 1 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_uc_feature_common.id
+  description              = "UC Feature host outbound port 1 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aws_uc_feature_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "uc_feature_host_outbound_tanium_2" {
-  description       = "UC Feature host outbound port 2 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_uc_feature_common.id
+  description              = "UC Feature host outbound port 2 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aws_uc_feature_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "uc_feature_host_inbound_tanium_1" {
-  description       = "UC Feature host inbound port 1 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_uc_feature_common.id
+  description              = "UC Feature host inbound port 1 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.aws_uc_feature_common.id
 }
 
 resource "aws_security_group_rule" "uc_feature_host_inbound_tanium_2" {
-  description       = "UC Feature host inbound port 2 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.aws_uc_feature_common.id
+  description              = "UC Feature host inbound port 2 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.aws_uc_feature_common.id
 }
 
 resource "aws_security_group_rule" "egress_aws_uc_feature_to_dks" {
